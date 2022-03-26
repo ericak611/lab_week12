a7bf0699e4e814380a6066f4208f2a9f5d8241da
Hyewon Kim <hkim444@my.bcit.ca>
2022-03-25 22:39:46 -0700

    deleted examples from lab_week12

--- examples/app.js
+++
@@ -1,5 +0,0 @@
-// const divone = document.querySelector('.one')
-// divone.classList.add('pink-background')
-
-// const divtwo = document.querySelector('.two')
-// divtwo.classList.toggle('blue-background')
