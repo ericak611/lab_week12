a2e29b4ae8c9c84156403c0916fc9262c20b7890
Hyewon Kim <hkim444@my.bcit.ca>
2022-03-25 21:40:04 -0700

    wrote a function that selects a single element and logs that element to the console.

---
+++ src/app.js
@@ -0,0 +1,5 @@
+function selectionOneFunction(element) {
+    const selectionOne = document.querySelector("element")
+    return selectionOne
+  }
+console.log(selectionOneFunction((".container light-theme")))  
