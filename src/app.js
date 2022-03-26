6240f2f9a069cfbc6012b5a31c6de9983058c9a1
Hyewon Kim <hkim444@my.bcit.ca>
2022-03-25 22:18:00 -0700

    Re-wrote code from week 10 lab question using functions.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,38 @@
+//part2
 function selectionOneFunction(element) {
     const selectionOne = document.querySelector("element")
     return selectionOne
   }
 console.log(selectionOneFunction((".container light-theme")))  
+
+//part3 
+const students = [
+    { name: "Tasha", grades: [ 80, 67, 75, 62 ] },
+    { name: "Tuvok", grades: [ 90, 88, 91, 92 ] },
+    { name: "Dax", grades: [ 88, 90, 89, 85 ] },
+    { name: "Neelix", grades: [ 60, 54, 48, 50 ] }
+  ]
+
+  function calculateGPA (average,studentsGetScholarship){
+    let overallGPA = 0
+    for (const number of average.grades){
+       overallGPA += number
+    }
+    const averageGPA = overallGPA / average.grades.length
+    if (averageGPA > 80){
+     const scholarshipStudent = { name: average.name, gradeAvg: averageGPA}
+     studentsGetScholarship.push(scholarshipStudent)
+    }
+
+ return overallGPA
+}
+
+function studentsWhoQualify (students){
+  const studentsGetScholarship = []
+  for (const average of students){
+    calculateGPA(average,studentsGetScholarship)
+
+}
+  return studentsGetScholarship
+}
+console.log(studentsWhoQualify(students))
